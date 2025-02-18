0728ec1206883686810ed9ac0077088013ac8b5d
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-02-18 11:21:45 +0000

    add UsersAcctouns context, CommentsPage  and styled Navbar

--- nc-news-react/src/App.jsx
+++ nc-news-react/src/App.jsx
@@ -1,20 +1,22 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import ArticlesList from './components/ArticlesList';
-import Navbar from './components/Navbar';
 import ArticlesPage from './pages/ArticlesPage';
 import SingleArticlePage from './pages/SingleArticlePage';
+import CommentsPage from './pages/CommentsPage';
+import { UsersAccountsProvider } from './context/UsersContext';
 
 function App() {
 
 
   return (
-    <Routes>
-      <Route path='/' element={<ArticlesPage />} />
-      <Route path='/:article_id' element={<SingleArticlePage />} />
+    <UsersAccountsProvider>
+      <Routes>
+        <Route path='/' element={<ArticlesPage />} />
+        <Route path='/:article_id' element={<SingleArticlePage />} />
+        <Route path='/:article_id/comments' element={<CommentsPage />} />
 
-    </Routes>
+      </Routes>
+    </UsersAccountsProvider>
   );
 }
 
---
+++ nc-news-react/src/pages/CommentsPage.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState, useContext } from "react";
+import Navbar from "../components/Navbar";
+import axios from 'axios';
+import { useLocation } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import CommentCard from "../components/CommentCard";
+import { UsersAccounts } from "../context/UsersContext";
+import Footer from "../components/Footer";
+
+
+const CommentsPage = () => {
+  const location = useLocation();
+  const article_id = location.pathname;
+  const [comments, setComments] = useState(null);
+  const { usersAccounts, setUsersAccounts } = useContext(UsersAccounts);
+
+  useEffect(() => {
+    axios.get(`https://andis-news-app.onrender.com/api/articles${article_id}`)
+      .then(resp => {
+        console.log(resp.data.comments);
+        setComments(resp.data.comments);
+      });
+    axios.get(`https://andis-news-app.onrender.com/api/users`).then(resp => {
+      console.log(resp.data);
+      setUsersAccounts(resp.data);
+    });
+  }, []);
+  return (<>
+    <div className="main-container w-11/12 h-auto m-auto">
+      <Navbar />
+      <h2 className="pr-4 mt-4 mr-auto">Comments:</h2>
+      <main className="flex flex-col justify-center items-center lg:grid lg:grid-cols-2">
+
+        {comments ? comments.map(comment => {
+          let userAvatar = '';
+          for (const obj of usersAccounts) {
+            if (obj.username === comment.author) { userAvatar = obj.avatar_url; }
+          }
+          return <CommentCard comment={comment} userAvatar={userAvatar} />;
+        }) : <Spinner />}
+      </main>
+    </div>
+    <Footer />
+  </>
+  );
+};
+export default CommentsPage;
+
