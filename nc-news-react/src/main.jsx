4c3f9517d82832766197f010f41e36e5ceae4716
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-02-21 12:05:36 +0000

    another main.jsx

--- nc-news-react/src/main.jsx
+++ nc-news-react/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
